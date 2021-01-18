f7677e5bdfbc8af25816b502afdae0358b3538f1
Lien Truong <lientruong714@gmail.com>
2021-01-18 10:01:19 -0800

    Stancl tenancy sample app.

--- app/Http/Controllers/Central/WelcomeController.php
+++ app/Http/Controllers/Central/WelcomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Central;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller {
 
     public function getIndex(Request $request) {
+
         return $this->view([
             'content' => 'pages/welcome/index',
         ]);
--- app/Http/Controllers/Tenant/WelcomeController.php
+++ app/Http/Controllers/Tenant/WelcomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Tenant;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller {
 
     public function getIndex(Request $request) {
+        dd(Auth::user());
         return $this->view([
             'content' => 'pages/welcome/index',
         ]);
